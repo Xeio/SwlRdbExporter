33586666b16bdade0f722b2ddc24b732f4a83048
Joshua Shaffer <xeio@live.com>
2018-04-07 00:17:28 -0400

    Add support for exporting dialog audio files

---
+++ RdbExporter/Exporters/VoiceoverExporter.cs
@@ -0,0 +1,55 @@
+﻿using RdbExporter.Entities;
+using RdbExporter.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RdbExporter.Exporters
+{
+    public class VoiceoverExporter : IExporter
+    {
+        public void RunExport(ExportParameters parameters)
+        {
+            Parallel.ForEach(parameters.RdbFileEntries, entry => Process(parameters, entry));
+        }
+
+        public void Process(ExportParameters parameters, IDBRIndexEntrty entry)
+        {
+            if (parameters.Arguments.Count < 0) throw new ArgumentException("RawFileExporter requires extension as first argument.");
+
+            using (var reader = entry.OpenEntryFile(parameters.SwlInstallDir))
+            {
+                for(int i = 0; i < entry.FileLength; i++)
+                {
+                    if(reader.ReadByte() == 'O')
+                    {
+                        //Search for Ogg file header (maybe a better way to do this? Need to understand the LIP file type better)
+                        if(reader.ReadByte() == 'g')
+                        {
+                            if (reader.ReadByte() == 'g')
+                            {
+                                if (reader.ReadByte() == 'S')
+                                {
+                                    reader.BaseStream.Seek(-4, SeekOrigin.Current);
+
+                                    string outputPath = Path.ChangeExtension(Path.Combine(parameters.ExportDirectory, entry.Id.ToString()), ".ogg");
+                                    using (var writer = File.OpenWrite(outputPath))
+                                    {
+                                        reader.BaseStream.CopyToLimited(writer, entry.FileLength - i);
+                                    }
+                                    break;
+                                }
+                                reader.BaseStream.Seek(-1, SeekOrigin.Current);
+                            }
+                            reader.BaseStream.Seek(-1, SeekOrigin.Current);
+                        }
+                        reader.BaseStream.Seek(-1, SeekOrigin.Current);
+                    }
+                }
+            }
+        }
+    }
+}
